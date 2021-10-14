98f75d3fa4b653ee8afc19e204d5da7a16709a35
felismao <gabriellajohari@gmail.com>
2021-10-14 18:19:29 +0700

    updates

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "volcanic_activity.json";
 vegaEmbed("#volcanic-activity", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 ="volcanic_number_year.json";
+vegaEmbed("#volcanic-number-peryear", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
